f1c2c7ba67c1ef95048a365179d075b13a331806
James Kraai <jkraai@mac.com>
2018-07-15 16:46:40 -0500

    Use body parser to parse application/vnd.api+json requests

--- api/app.ts
+++ api/app.ts
@@ -1,5 +1,11 @@
+import bodyParser = require("body-parser");
 import express = require("express");
 import { movieController } from "./controllers/movie.controller";
 
 export const app = express();
+
+app.use(bodyParser.raw({
+    type: 'application/vnd.api+json',
+}));
+
 app.use('/movies', movieController);
