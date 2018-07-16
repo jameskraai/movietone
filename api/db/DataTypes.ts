262a18bb1aa1f8737efb9eeb5a396f3b62a40e44
James Kraai <jkraai@mac.com>
2018-07-15 20:41:23 -0500

    Add a file to contain the shape of the data we're storing, for now just Movie

---
+++ api/db/DataTypes.ts
@@ -0,0 +1,10 @@
+export interface IMovie {
+    id: string;
+    title: string;
+    genre?: string;
+    actors?: string;
+    year: number;
+    rating?: number;
+    createdAt: string;
+    updated: string;
+}
