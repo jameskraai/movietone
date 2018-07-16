58cfd107f7b218100728355d4976a8bb22da310c
James Kraai <jkraai@mac.com>
2018-07-15 21:31:28 -0500

    Impelement getMovie endpoint

---
+++ api/routers/movies/getMovie.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import { MysqlError } from "mysql";
+import { db } from "../../db";
+import { IMovie } from "../../db/DataTypes";
+
+export const getMovie = (req: Request, res: Response) => {
+    db.query({
+        sql: "SELECT * FROM movies WHERE id = ?",
+        values: [req.params.movieId]
+    }, (err: MysqlError | null, results: IMovie[]) => {
+        res.status(200);
+        res.contentType('application/json');
+
+        if (err) {
+            res.send({
+                errors: [
+                    {
+                        description: err,
+                        title: "Error getting Movie",
+                    }
+                ]
+            });
+            return;
+        }
+
+        if (results.length > 0) {
+            return res.send({
+                data: {
+                    attributes: {
+                        actors: results[0].actors,
+                        createdAt: results[0].createdAt,
+                        genre: results[0].genre,
+                        rating: results[0].rating,
+                        title: results[0].title,
+                        updated: results[0].updated,
+                        year: results[0].year,
+                    },
+                    id: results[0].id,
+                    type: "movies"
+                }
+            });
+        }
+
+        res.send({
+            errors: [
+                {
+                    description: `No Movies found for the given id: ${req.params.movieId}`,
+                    title: "No Movie Found",
+                }
+            ]
+        });
+    });
+};
--- api/routers/movies/index.ts
+++ api/routers/movies/index.ts
@@ -1,14 +1,13 @@
 import { Request, Response, Router } from "express";
 import { createMovie } from "./createMovie";
+import { getMovie } from "./getMovie";
 import { getMovies } from "./getMovies"
 
 export const movieController: Router = Router();
 
 movieController.get('/', getMovies);
 
-movieController.get('/:movieId', () => {
-    //
-});
+movieController.get('/:movieId', getMovie);
 
 movieController.post('/:movieId', createMovie);
 
