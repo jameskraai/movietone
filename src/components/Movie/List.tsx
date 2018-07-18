8c7c604aa3ee005874a7c1c39e2c70278e88f124
James Kraai <jkraai@mac.com>
2018-07-17 20:18:22 -0500

    Connect list to store and show movies

--- src/components/Movie/List.tsx
+++ src/components/Movie/List.tsx
@@ -1,4 +1,22 @@
 import * as React from "react";
-export const List = () => (
-    <h1>Movie List</h1>
+import { connect } from "react-redux";
+import { MovieList, MovieMap } from "../../../api/db/DataTypes";
+import { getList } from "../../store/movies/selectors";
+import { IState } from "../../store/State";
+
+interface IProps {
+    list: MovieList;
+}
+
+export const List = (props: IProps) => (
+    <section>
+        <h1>Movie List</h1>
+        {props.list.map((movie: MovieMap) => (<p>{movie.get("title")}</p>))}
+    </section>
 );
+
+const mapStateToProps = (state: IState): IProps => ({
+    list: getList(state),
+});
+
+export default connect(mapStateToProps, null)(List);
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import { List as MovieList } from "../components/Movie/List";
-
+import MovieList from "../components/Movie/List";
 
 export default () => (
     <BrowserRouter>
